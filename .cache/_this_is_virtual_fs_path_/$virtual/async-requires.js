bcdc4e62456f14ba67356115f4b0b2e24898af59
minheonjun <tamunavollt1@gmail.com>
2023-08-08 15:51:30 +0900

    code challenge

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,19 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx?export=default" /* webpackChunkName: "component---src-pages-404-tsx" */),
+  "component---src-pages-about-us-tsx": () => import("./../../../src/pages/about-us.tsx?export=default" /* webpackChunkName: "component---src-pages-about-us-tsx" */),
+  "component---src-pages-blog-index-tsx": () => import("./../../../src/pages/blog/index.tsx?export=default" /* webpackChunkName: "component---src-pages-blog-index-tsx" */),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-bye-index-mdx": () => import("./../../../src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/bye/index.mdx&export=default" /* webpackChunkName: "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-bye-index-mdx" */),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-hello-index-mdx": () => import("./../../../src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/hello/index.mdx&export=default" /* webpackChunkName: "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-hello-index-mdx" */)
+}
+
+
+
+exports.head = {
+  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx?export=head" /* webpackChunkName: "component---src-pages-404-tsxhead" */),
+  "component---src-pages-about-us-tsx": () => import("./../../../src/pages/about-us.tsx?export=head" /* webpackChunkName: "component---src-pages-about-us-tsxhead" */),
+  "component---src-pages-blog-index-tsx": () => import("./../../../src/pages/blog/index.tsx?export=head" /* webpackChunkName: "component---src-pages-blog-index-tsxhead" */),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-bye-index-mdx": () => import("./../../../src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/bye/index.mdx&export=head" /* webpackChunkName: "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-bye-index-mdxhead" */),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-hello-index-mdx": () => import("./../../../src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/hello/index.mdx&export=head" /* webpackChunkName: "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-hello-index-mdxhead" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/.cache/dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/src/pages/404.tsx")),
+  "component---src-pages-about-us-tsx": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/src/pages/about-us.tsx")),
+  "component---src-pages-blog-index-tsx": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/src/pages/blog/index.tsx")),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-bye-index-mdx": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/bye/index.mdx")),
+  "component---src-pages-blog-mdx-frontmatter-slug-tsx-content-file-path-blog-posts-hello-index-mdx": preferDefault(require("/Users/minheonjun/Desktop/Coding/ReactLab/src/pages/blog/{mdx.frontmatter__slug}.tsx?__contentFilePath=/Users/minheonjun/Desktop/Coding/ReactLab/blog-posts/hello/index.mdx"))
+}
+
